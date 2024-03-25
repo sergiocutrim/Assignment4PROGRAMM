c6bc058cd2b76c3145e90a27331286a4c2fc81a9
sergiocutrim <97315931+sergiocutrim@users.noreply.github.com>
2024-03-25 10:22:04 -0400

    Delete Java-Assignment 4/MethodsTask.java

--- Java-Assignment 4/MethodsTask.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class MethodsTask {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        // Ask the user to input a number between 1 and 10
-        System.out.print("Please enter a number between 1 and 10: ");        
-    }
-}
